871216a4523873403a095dcf23d20f6908aaaaaa
RHETT CHASE <rhettjchase@gmail.com>
2024-02-13 18:32:38 -0800

    creates SnackListView

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SnackListView
+from .views import SnackListView, SnackDetailView
 
 urlpatterns=[
     path('', SnackListView.as_view(), name='snack_list'),
+    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail'),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Snack
 
-class HomePageView(TemplateView):
-    template_name='home.html'
-
-class SnackListView(TemplateView):
+class SnackListView(ListView):
     template_name='snack_list.html'
+    model = Snack
+
+class SnackDetailView(DetailView):
+  template_name = 'snack_detail.html'
+  model = Snack
+    
