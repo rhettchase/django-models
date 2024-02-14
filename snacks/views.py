b0412cdff4b8e9b92a7a06c07ff63a5ec7981eb3
RHETT CHASE <rhettjchase@gmail.com>
2024-02-13 19:17:00 -0800

    updates styling

--- snacks/views.py
+++ snacks/views.py
@@ -4,6 +4,7 @@ from .models import Snack
 class SnackListView(ListView):
     template_name='snack_list.html'
     model = Snack
+    context_object_name = 'snacks'
 
 class SnackDetailView(DetailView):
   template_name = 'snack_detail.html'
